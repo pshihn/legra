51892d8cfd794b679542d7b140df63d74a90ac27
Preet Shihn <preetshihn@gmail.com>
2019-11-06 14:03:49 -0800

    unfilled rectangles

--- src/legra-core.ts
+++ src/legra-core.ts
@@ -1,3 +1,5 @@
+export type Point = [number, number];
+
 export interface BrickStyle {
   brickSize?: number;
   color?: string;
@@ -10,7 +12,17 @@ export interface BrickStyleResolved extends BrickStyle {
   elevation: number;
 }
 
-function drawBrick(ctx: CanvasRenderingContext2D, x: number, y: number, style: BrickStyleResolved) {
+const radiusCache = new Map<number, number>();
+function calculateRadius(size: number): number {
+  if (radiusCache.has(size)) {
+    return radiusCache.get(size)!;
+  }
+  const r = Math.min(24, (size * 0.5) / 2);
+  radiusCache.set(size, r);
+  return r;
+}
+
+function _drawBrick(ctx: CanvasRenderingContext2D, x: number, y: number, style: BrickStyleResolved) {
   const { brickSize, color } = style;
   ctx.save();
   ctx.fillStyle = color;
@@ -29,61 +41,97 @@ function drawBrick(ctx: CanvasRenderingContext2D, x: number, y: number, style: B
   ctx.restore();
 }
 
-const radiusCache = new Map<number, number>();
-function calculateRadius(size: number): number {
-  if (radiusCache.has(size)) {
-    return radiusCache.get(size)!;
-  }
-  const r = Math.min(24, (size * 0.5) / 2);
-  radiusCache.set(size, r);
-  return r;
+function drawBrick(i: number, j: number, ctx: CanvasRenderingContext2D, style: BrickStyleResolved) {
+  const size = style.brickSize;
+  _drawBrick(ctx, i * size, j * size, style);
 }
 
-export function rectangle(x: number, y: number, width: number, height: number, ctx: CanvasRenderingContext2D, style: BrickStyleResolved) {
+function drawBrickList(points: Point[], ctx: CanvasRenderingContext2D, style: BrickStyleResolved) {
   const size = style.brickSize;
-  for (let i = 0; i < width; i++) {
-    for (let j = 0; j < height; j++) {
-      drawBrick(ctx, (x + i) * size, (y + j) * size, style);
+  points.forEach((p) => {
+    _drawBrick(ctx, p[0] * size, p[1] * size, style);
+  });
+}
+
+function _line(x1: number, y1: number, x2: number, y2: number, used = new Set<string>()): Point[] {
+  const points: Point[] = [];
+
+  const pushToPoints = (p: Point) => {
+    const key = p.join(',');
+    if (!used.has(key)) {
+      used.add(key);
+      points.push(p);
     }
-  }
-}
+  };
 
-export function line(x1: number, y1: number, x2: number, y2: number, ctx: CanvasRenderingContext2D, style: BrickStyleResolved) {
-  const size = style.brickSize;
   if (x1 === x2) {
     const min = Math.min(y1, y2);
     const max = Math.max(y1, y2);
     for (let j = min; j <= max; j++) {
-      drawBrick(ctx, x1 * size, j * size, style);
+      pushToPoints([x1, j]);
     }
   } else {
     const dy = y2 - y1;
     const dx = x2 - x1;
     const m = dy / dx;
     const c = y1 - (m * x1);
-    const used = new Set<string>();
     if (Math.abs(dx) >= Math.abs(dy)) {
       const min = Math.min(x1, x2);
       const max = Math.max(x1, x2);
       for (let i = min; i <= max; i++) {
         const j = Math.round((m * i) + c);
-        const key = `${i},${j}`;
-        if (!used.has(key)) {
-          used.add(key);
-          drawBrick(ctx, i * size, j * size, style);
-        }
+        pushToPoints([i, j]);
       }
     } else {
       const min = Math.min(y1, y2);
       const max = Math.max(y1, y2);
       for (let j = min; j <= max; j++) {
         const i = Math.round((j - c) / m);
-        const key = `${i},${j}`;
-        if (!used.has(key)) {
-          used.add(key);
-          drawBrick(ctx, i * size, j * size, style);
-        }
+        pushToPoints([i, j]);
       }
     }
   }
+  return points;
+}
+
+export function rectangle(x: number, y: number, width: number, height: number, filled: boolean, ctx: CanvasRenderingContext2D, style: BrickStyleResolved) {
+  if (filled) {
+    for (let i = 0; i < width; i++) {
+      for (let j = 0; j < height; j++) {
+        drawBrick(x + i, y + j, ctx, style);
+      }
+    }
+  } else {
+    const used = new Set<string>();
+    if (width > 0 && height > 0) {
+      let bricks = _line(x, y, x + width - 1, y, used);
+      if (height > 2) {
+        bricks = [
+          ...bricks,
+          ..._line(x, y + 1, x, y + height - 2, used),
+          ..._line(x + width - 1, y + 1, x + width - 1, y + height - 2, used)
+        ];
+      }
+      if (height > 1) {
+        bricks = [...bricks, ..._line(x, y + height - 1, x + width - 1, y + height - 1, used)];
+      }
+      drawBrickList(bricks, ctx, style);
+    }
+  }
+}
+
+export function line(x1: number, y1: number, x2: number, y2: number, ctx: CanvasRenderingContext2D, style: BrickStyleResolved) {
+  drawBrickList(_line(x1, y1, x2, y2), ctx, style);
+}
+
+export function linearPath(points: Point[], ctx: CanvasRenderingContext2D, style: BrickStyleResolved) {
+  const used = new Set<string>();
+  let bricks: Point[] = [];
+  for (let i = 0; i < points.length - 1; i++) {
+    const [x1, y1] = points[i];
+    const [x2, y2] = points[i + 1];
+    const bp = _line(x1, y1, x2, y2, used);
+    bricks = [...bricks, ...bp];
+  }
+  drawBrickList(bricks, ctx, style);
 }
